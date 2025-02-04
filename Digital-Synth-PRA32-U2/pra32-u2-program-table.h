ccfb782c53ed5635184ef76652ca000b08d7d13c
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2025-02-04 21:07:19 +0900

    Change the Preset programs (4)

--- Digital-Synth-PRA32-U2/pra32-u2-program-table.h
+++ Digital-Synth-PRA32-U2/pra32-u2-program-table.h
@@ -3,7 +3,7 @@
 const uint8_t PROGRAM_NUMBER_DEFAULT = 8;
 
 // Preset                                         #0   #1   #2   #3   #4   #5   #6   #7     #8   #9   #10  #11  #12  #13  #14  #15  
-const uint8_t g_preset_table_OSC_1_WAVE     [] = {127, 127, 127, 51 , 127, 25 , 127, 0  ,   127, 127, 127, 51 , 127, 25 , 127, 0  };
+const uint8_t g_preset_table_OSC_1_WAVE     [] = {127, 127, 127, 102, 127, 76 , 127, 0  ,   127, 127, 127, 102, 127, 76 , 127, 0  };
 const uint8_t g_preset_table_OSC_1_SHAPE    [] = {0  , 0  , 0  , 64 , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 64 , 0  , 0  , 0  , 0  };
 const uint8_t g_preset_table_OSC_1_MORPH    [] = {64 , 64 , 0  , 64 , 64 , 108, 64 , 0  ,   64 , 64 , 0  , 64 , 64 , 108, 64 , 0  };
 const uint8_t g_preset_table_MIXER_SUB_OSC  [] = {127, 64 , 127, 64 , 127, 64 , 64 , 64 ,   127, 64 , 127, 64 , 127, 64 , 64 , 64 };
