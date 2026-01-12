b5bb1c816db5fa0eec1000033397737d88496b6b
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2026-01-12 14:16:14 +0900

    Improve the smoothing of the filter

--- Digital-Synth-PRA32-U2/pra32-u2-filter.h
+++ Digital-Synth-PRA32-U2/pra32-u2-filter.h
@@ -173,10 +173,7 @@ private:
     cutoff_target = (cutoff_target < 0) * cutoff_target + ((254 << 2) + 1);
     cutoff_target = (cutoff_target > 0) * cutoff_target;
 
-    for (uint32_t i = 0; i < (4 * 2); ++i) {
-      m_cutoff_current += (m_cutoff_current < cutoff_target);
-      m_cutoff_current -= (m_cutoff_current > cutoff_target);
-    }
+    m_cutoff_current = cutoff_target;
 
     m_resonance_current += (m_resonance_current < m_resonance_target);
     m_resonance_current -= (m_resonance_current > m_resonance_target);
