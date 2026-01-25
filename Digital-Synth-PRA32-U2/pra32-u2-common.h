879a7c4e532e6dccbbe933d21d548460008cc04f
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2026-01-26 06:05:29 +0900

    Improve the code

--- Digital-Synth-PRA32-U2/pra32-u2-common.h
+++ Digital-Synth-PRA32-U2/pra32-u2-common.h
@@ -12,23 +12,7 @@ static INLINE uint8_t high_byte(uint16_t x) {
   return (x >> 8) & 0xFF;
 }
 
-static INLINE int8_t high_sbyte(int16_t x) {
-  return (x >> 8) & 0xFF;
-}
-
-static INLINE int32_t mul_s32_s16_h32(int32_t x, int16_t y) {
-  return (static_cast<int64_t>(x) * y) >> 16;
-}
-
 static INLINE int32_t mul_s32_u16_h32(int32_t x, uint16_t y) {
-  return (static_cast<int64_t>(x) * y) >> 16;
-}
-
-static INLINE int32_t mul_u32_s16_h32(uint32_t x, int16_t y) {
-  return (static_cast<int64_t>(x) * y) >> 16;
-}
-
-static INLINE uint32_t mul_u32_u16_h32(uint32_t x, uint16_t y) {
   return (static_cast<int64_t>(x) * y) >> 16;
 }
 
