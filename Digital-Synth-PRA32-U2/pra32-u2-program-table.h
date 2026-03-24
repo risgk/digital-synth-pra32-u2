c6974ead1ae82760cf88c5ded3a3a712ec7bb0a6
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2026-03-24 23:15:32 +0900

    Modify the Presets and the Editor Presets (5)

--- Digital-Synth-PRA32-U2/pra32-u2-program-table.h
+++ Digital-Synth-PRA32-U2/pra32-u2-program-table.h
@@ -13,9 +13,9 @@ const uint8_t g_preset_table_MIXER_OSC_MIX  [] = {64 , 0  , 64 , 0  , 64 , 0  , 
 const uint8_t g_preset_table_OSC_2_COARSE   [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 const uint8_t g_preset_table_OSC_2_PITCH    [] = {72 , 72 , 72 , 72 , 66 , 72 , 72 , 64 ,   72 , 72 , 72 , 72 , 66 , 72 , 72 , 64 };
 
-const uint8_t g_preset_table_FILTER_CUTOFF  [] = {112, 112, 88 , 127, 64 , 112, 52 , 127,   112, 112, 88 , 127, 64 , 112, 52 , 127};
+const uint8_t g_preset_table_FILTER_CUTOFF  [] = {112, 112, 88 , 127, 88 , 112, 52 , 127,   112, 112, 88 , 127, 88 , 112, 52 , 127};
 const uint8_t g_preset_table_FILTER_RESO    [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 0  ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 0  };
-const uint8_t g_preset_table_FILTER_EG_AMT  [] = {40 , 64 , 64 , 64 , 88 , 64 , 88 , 64 ,   40 , 64 , 64 , 64 , 88 , 64 , 88 , 64 };
+const uint8_t g_preset_table_FILTER_EG_AMT  [] = {40 , 64 , 64 , 64 , 76 , 64 , 88 , 64 ,   40 , 64 , 64 , 64 , 76 , 64 , 88 , 64 };
 const uint8_t g_preset_table_FILTER_KEY_TRK [] = {96 , 96 , 96 , 96 , 96 , 96 , 96 , 64 ,   96 , 96 , 96 , 96 , 96 , 96 , 96 , 64 };
 
 const uint8_t g_preset_table_EG_ATTACK      [] = {96 , 32 , 32 , 32 , 32 , 32 , 32 , 0  ,   96 , 32 , 32 , 32 , 32 , 32 , 32 , 0  };
