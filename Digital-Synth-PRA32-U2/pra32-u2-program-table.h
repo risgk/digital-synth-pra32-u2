19913fcffb61a510756ce3cd764989d3500df5dc
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2026-03-29 07:51:52 +0900

    Modify the Presets and the Editor Presets for Mono

--- Digital-Synth-PRA32-U2/pra32-u2-program-table.h
+++ Digital-Synth-PRA32-U2/pra32-u2-program-table.h
@@ -13,10 +13,10 @@ const uint8_t g_preset_table_MIXER_OSC_MIX  [] = {64 , 0  , 64 , 0  , 64 , 0  , 
 const uint8_t g_preset_table_OSC_2_COARSE   [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 const uint8_t g_preset_table_OSC_2_PITCH    [] = {72 , 72 , 72 , 72 , 66 , 72 , 66 , 64 ,   72 , 72 , 72 , 72 , 66 , 72 , 66 , 64 };
 
-const uint8_t g_preset_table_FILTER_CUTOFF  [] = {112, 112, 88 , 127, 88 , 112, 52 , 127,   112, 112, 88 , 127, 88 , 112, 52 , 127};
+const uint8_t g_preset_table_FILTER_CUTOFF  [] = {112, 112, 88 , 127, 88 , 112, 40 , 127,   112, 112, 88 , 127, 88 , 112, 40 , 127};
 const uint8_t g_preset_table_FILTER_RESO    [] = {48 , 48 , 48 , 48 , 48 , 48 , 80 , 0  ,   48 , 48 , 48 , 48 , 48 , 48 , 80 , 0  };
 const uint8_t g_preset_table_FILTER_EG_AMT  [] = {40 , 64 , 64 , 64 , 76 , 64 , 88 , 64 ,   40 , 64 , 64 , 64 , 76 , 64 , 88 , 64 };
-const uint8_t g_preset_table_FILTER_KEY_TRK [] = {96 , 96 , 96 , 96 , 96 , 96 , 96 , 64 ,   96 , 96 , 96 , 96 , 96 , 96 , 96 , 64 };
+const uint8_t g_preset_table_FILTER_KEY_TRK [] = {96 , 96 , 96 , 96 , 64 , 64 , 64 , 64 ,   96 , 96 , 96 , 96 , 64 , 64 , 64 , 64 };
 
 const uint8_t g_preset_table_EG_ATTACK      [] = {96 , 32 , 32 , 32 , 32 , 32 , 32 , 0  ,   96 , 32 , 32 , 32 , 32 , 32 , 32 , 0  };
 const uint8_t g_preset_table_EG_DECAY       [] = {96 , 32 , 96 , 32 , 32 , 96 , 100, 0  ,   96 , 32 , 96 , 32 , 32 , 96 , 100, 0  };
@@ -63,7 +63,7 @@ const uint8_t g_preset_table_OSC_SAW_W_MODE [] = {127, 127, 127, 127, 127, 127, 
 
 
 
-const uint8_t g_preset_table_CHORUS_MIX     [] = {127, 127, 127, 127, 127, 127, 127, 0  ,   127, 127, 127, 127, 127, 127, 127, 0  };
+const uint8_t g_preset_table_CHORUS_MIX     [] = {127, 127, 127, 127, 64 , 64 , 64 , 0  ,   127, 127, 127, 127, 64 , 64 , 64 , 0  };
 
 const uint8_t g_preset_table_CHORUS_RATE    [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 const uint8_t g_preset_table_CHORUS_DEPTH   [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
