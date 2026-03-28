e4e0530fcfb7d2ea382aa7dd23709a815ec675b1
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2026-03-29 08:01:16 +0900

    Modify the Presets and the Editor Presets for Mono (2)

--- Digital-Synth-PRA32-U2/pra32-u2-program-table.h
+++ Digital-Synth-PRA32-U2/pra32-u2-program-table.h
@@ -5,7 +5,7 @@ const uint8_t PROGRAM_NUMBER_DEFAULT = 0;
 // Preset                                         #0   #1   #2   #3   #4   #5   #6   #7     #8   #9   #10  #11  #12  #13  #14  #15  
 const uint8_t g_preset_table_OSC_1_WAVE     [] = {0  , 0  , 76 , 127, 0  , 25 , 0  , 0  ,   0  , 0  , 76 , 127, 0  , 25 , 0  , 0  };
 const uint8_t g_preset_table_MIXER_SUB_OSC  [] = {64 , 64 , 64 , 64 , 127, 96 , 127, 64 ,   64 , 64 , 64 , 64 , 127, 96 , 127, 64 };
-const uint8_t g_preset_table_OSC_1_SHAPE    [] = {64 , 64 , 0  , 0  , 64 , 64 , 64 , 0  ,   64 , 64 , 0  , 0  , 64 , 64 , 64 , 0  };
+const uint8_t g_preset_table_OSC_1_SHAPE    [] = {64 , 64 , 0  , 0  , 64 , 64 , 0  , 0  ,   64 , 64 , 0  , 0  , 64 , 64 , 0  , 0  };
 const uint8_t g_preset_table_OSC_1_MORPH    [] = {0  , 127, 108, 64 , 0  , 127, 0  , 0  ,   0  , 127, 108, 64 , 0  , 127, 0  , 0  };
 
 const uint8_t g_preset_table_OSC_2_WAVE     [] = {0  , 0  , 0  , 0  , 0  , 25 , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 25 , 0  , 0  };
@@ -63,7 +63,7 @@ const uint8_t g_preset_table_OSC_SAW_W_MODE [] = {127, 127, 127, 127, 127, 127, 
 
 
 
-const uint8_t g_preset_table_CHORUS_MIX     [] = {127, 127, 127, 127, 64 , 64 , 64 , 0  ,   127, 127, 127, 127, 64 , 64 , 64 , 0  };
+const uint8_t g_preset_table_CHORUS_MIX     [] = {127, 127, 127, 127, 127, 127, 127, 0  ,   127, 127, 127, 127, 127, 127, 127, 0  };
 
 const uint8_t g_preset_table_CHORUS_RATE    [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 const uint8_t g_preset_table_CHORUS_DEPTH   [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
