0da5253347fd5bdeed89fca0117c05b416e0ba9b
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2026-08-17 21:51:37 +0900

    Refactor code (2)

--- Digital-Synth-PRA32-U2/pra32-u2-generate-eg-table.rb
+++ Digital-Synth-PRA32-U2/pra32-u2-generate-eg-table.rb
@@ -5,14 +5,14 @@ $file = File.open("pra32-u2-eg-table.h", "wb")
 $file.printf("#pragma once\n\n")
 
 $file.printf("int32_t g_eg_attack_decay_release_coef_table[] = {\n  ")
-(0..127 * (2 ** EG_TABLE_EXT_BITS) + 16 * (2 ** EG_TABLE_EXT_BITS)).each do |i|
+(0..128 * (2 ** EG_TABLE_EXT_BITS) + 16 * (2 ** EG_TABLE_EXT_BITS)).each do |i|
   time = i
   eg_coef = (0.5 ** (1.0 / ((0.2 / 10.0) * (SAMPLING_RATE / 4) *
              (10.0 ** ((time - (64.0 * (2 ** EG_TABLE_EXT_BITS))) /
               (32.0 * (2 ** EG_TABLE_EXT_BITS)))))) * 0x40000000).round
 
   $file.printf("%10d,", eg_coef)
-  if i == 127 * (2 ** EG_TABLE_EXT_BITS) + 16 * (2 ** EG_TABLE_EXT_BITS)
+  if i == 128 * (2 ** EG_TABLE_EXT_BITS) + 16 * (2 ** EG_TABLE_EXT_BITS)
     $file.printf("\n")
   elsif i % (8 * (2 ** EG_TABLE_EXT_BITS)) == ((8 * (2 ** EG_TABLE_EXT_BITS)) - 1)
     $file.printf("\n  ")
